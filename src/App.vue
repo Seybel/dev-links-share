edbd63f2b78c474ded395a83e3753f44567c032f
mctursh <ayoade@seamlesshr.com>
2023-07-24 12:45:52 +0100

    added pages and setup colors config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
+<div>
+  <router-view></router-view>
+</div>
+  <!-- <div>
     <h1 class="text-3xl font-bold underline">HelloWorld</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -12,7 +15,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
