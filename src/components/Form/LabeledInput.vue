aca737fca892cd9c14a1503b17afa8117a02d9f0
mctursh <ayoade@seamlesshr.com>
2023-07-26 03:29:14 +0100

    added error state to input

--- src/components/Form/LabeledInput.vue
+++ src/components/Form/LabeledInput.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="flex flex-col gap-y-1">
-        <label :for="id" class="text-dark-grey text-sm font-normal">{{ label }}</label>
+        <label :for="id" 
+        :class="`text-dark-grey text-sm font-normal
+            ${isError && 'text-red'}
+        `"
+        >{{ label }}</label>
         <div 
-            :class="`w-[100%] min-w-[440px] max-w-max flex items-center py-3 px-3.5 gap-x-4 text-base color-dark-grey rounded-lg placeholder-current::placeholder border border-border bg-white
+            :class="`w-[100%] min-w-[440px] max-w-max flex items-center justify-between py-3 px-3.5 gap-x-4 text-base color-dark-grey rounded-lg placeholder-current::placeholder border border-border bg-white
                 ${isFocused && 'outline-none border-purple shadow-light-purple'}
+                ${isError && 'border-red shadow-none'}
             `"
             >
-            <slot name="icon"></slot>
-            <input 
-                :id="id"
-                class="text-dark-grey bg-white border-none outline-none placeholder-dark-grey placeholder-opacity-50"
-                :type="type" @change="handleChange"
-                :placeholder="placeholder"
-                @focus="toggleFocus"
-                @blur="toggleFocus"
-            >
+            <div class="flex justify-between align-center gap-x-4">
+                <slot name="icon"></slot>
+                <input 
+                    :id="id"
+                    class="text-dark-grey bg-white border-none outline-none placeholder-dark-grey placeholder-opacity-50"
+                    :type="type" @change="handleChange"
+                    :placeholder="placeholder"
+                    @focus="toggleFocus"
+                    @blur="toggleFocus"
+                >
+            </div>
+            <p v-if="isError" class="text-red text-sm font-normal">{{ errorMsg }}</p>
         </div>
     </div>
 </template>
@@ -24,7 +32,9 @@ defineProps<{
     label: string,
     id: string,
     type: string,
-    placeholder: string
+    placeholder: string,
+    errorMsg: string,
+    isError: boolean
 }>();
 
 import { ref } from 'vue'
--- src/components/UI/LoginForm.vue
+++ src/components/UI/LoginForm.vue
@@ -7,12 +7,12 @@
         <div class="bg-white p-14 rounded flex align-center justify-start self-center flex-col gap-y-2 w-[fit-content] ">
             <h1 class="text-[2em] font-bold">Login</h1>
             <p class="text-base font-normal text-grey mb-8">Add your details below to get back into the app</p>
-            <LabeledInput class="mb-6" type="text" label="Email address" id="email">
+            <LabeledInput :is-error="false" errorMsg="Can’t be empty" placeholder="e.g. alex@email.com" class="mb-6" type="text" label="Email address" id="email">
                 <template v-slot:icon>
                     <img src="../../assets/icons/icon-email.svg" alt="email">
                 </template>
             </LabeledInput>
-            <LabeledInput class="mb-3" type="password" label="Password" id="password">
+            <LabeledInput :is-error="true" errorMsg="Please check again" placeholder="Enter your password" class="mb-3" type="password" label="Password" id="password">
                 <template v-slot:icon>
                     <img src="../../assets/icons/icon-password.svg" alt="password">
                 </template>
