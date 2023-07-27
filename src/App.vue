5cf62c12e4932be2be685353b0f521ad01db1aaf
mctursh <ayoade@seamlesshr.com>
2023-07-27 15:39:08 +0100

    complete header UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <Navbar></Navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
 // import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from './components/UI/Navbar.vue';
 </script>
 
 <style scoped>
